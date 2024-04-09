16233088d0e97d44523eef3343e0430aaf5d721f
Alexander Mikhailov <zelomod@mail.ru>
2024-04-09 11:51:48 +0300

    update
    add program

---
+++ TicTacToe(no classes)/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Введите первый символ: ");
+char firstPlayerSymbol = Convert.ToChar(Console.ReadLine());
+
+if (firstPlayerSymbol != 'X' || firstPlayerSymbol != '0')
+    return;
+
+char currentPlayerSymbol = firstPlayerSymbol;
+
+char[,] field =
+{
+    {'-','-','-', },
+    {'-','-','-', },
+    {'-','-','-', }
+};
+
+while (true)
+{
+    Console.Clear();
+
+    Draw();
+
+    var position = GetPosition();
+
+
+
+
+}
+
+void Draw()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            Console.WriteLine(field[i, j]);
+        }
+
+        Console.WriteLine();
+    }
+}
+
+(int, int) GetPosition()
+{
+    var position = Console.ReadLine();
+
+    var positionOne = position[0];
+    var positionTwo = ;
+
+    return (positionOne, positionTwo);
+}
+
